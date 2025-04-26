72e91da1c2589422f972a972ebf86c826b7ad088
devzone-creator <munkailasule3@gmail.com>
2025-04-26 04:24:57 +0000

    first-commit

---
+++ public/script.js
@@ -0,0 +1,17 @@
+//function toggleMenu(){
+//    const menuList = document.getElementById('menu');
+//    menuList.classList,toggle("active")
+//}
+
+const a = document.getElementById('amount')
+const t = document.getElementById('term');
+
+if(a && t){
+    
+}
+
+
+function toggleNewsLetter(){
+    const form = document.getElementById('loanform');
+    form.classList.toggle("active");
+}
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(1000, () => {
+    console.log('Server is ready . ...')
+})
